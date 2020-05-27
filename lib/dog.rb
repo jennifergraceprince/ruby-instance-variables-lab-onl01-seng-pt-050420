7c1bafadfad83d163bd1067555df07c5655ec4e9
Jennifer Prince <jennifergraceprince@gmail.com>
2020-05-27 07:28:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog 
   def name=(dog_name)
     @this_dogs_name = 
-    dog_name
+    dogs_name
   end
   
   def name
@@ -11,4 +11,4 @@ end
 
 lassie = Dog.new 
 lassie.name = "Lassie"
-lassie.name
+puts lassie.name
